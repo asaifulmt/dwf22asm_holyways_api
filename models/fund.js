a2db389126bf9135fabd57a4f90b24874f8ce951
asaifulmt <asaifulmt@gmail.com>
2021-05-03 01:36:45 +0700

    remove unused foreign key

--- models/fund.js
+++ models/fund.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate({ user, userDonate }) {
       // define association here
-      this.belongsToMany(user, { as: 'usersDonate', through: userDonate, foreignKey: 'userId' })
+      this.belongsToMany(user, { as: 'usersDonate', through: userDonate })
     }
     toJSON(){
       return {...this.get(), userId: undefined, createdAt: undefined, updatedAt: undefined}
